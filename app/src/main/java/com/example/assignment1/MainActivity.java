c3073ff742f530d3549a302737680fdf796f63a0
Fahim <fahimahmed0632@gmail.com>
2024-10-05 01:45:38 +0600

    Assingment 1

---
+++ app/src/main/java/com/example/assignment1/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.assignment1;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView textView = findViewById(R.id.textView);
+        ImageView imageView = findViewById(R.id.imageView);
+        Button button = findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Button clicked!", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
